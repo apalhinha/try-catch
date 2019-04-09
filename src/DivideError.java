0e904255cf5ee10ccad4dfa74619021f9987bce4
apalhinha <andre.palhinha@gmail.com>
2019-04-09 17:40:31 +0100

    wrapper but incomplete

--- src/DivideError.java
+++ src/DivideError.java
@@ -3,4 +3,15 @@ public class DivideError {
 	public int uncontrolled(int a) {
 		return 1/a;
 	}
+	
+	public int justwrap(int a) {
+		int res;
+		try {
+			res=uncontrolled(a);
+		} finally {
+			System.out.println("Got an error");
+			res=0;
+		}
+		return res;
+	}
 }
--- src/TryCatchApp.java
+++ src/TryCatchApp.java
@@ -4,17 +4,17 @@ public class TryCatchApp {
 
 	public static void main(String[] args) throws IOException {
 		int ch, calc;
-		System.out.println("1: Devide by zero");
-		System.out.println("2: Open inexistent file");
+		System.out.println("1: Uncontrolled");
+		System.out.println("2: Catch not that smart");
 		
 		ch = System.in.read();
+		DivideError d = new DivideError();
 		switch (ch) {
 		case '1':
-			DivideError d = new DivideError();
 			calc=d.uncontrolled(0);
 			break;
 		case '2':
-			System.out.println("2 not implemented");
+			calc=d.justwrap(0);
 			break;
 		}
 		System.out.println("Main is done");
