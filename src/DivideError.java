7953dc3f60ac1184312fb2775dd520f46f7c6ac7
apalhinha <andre.palhinha@gmail.com>
2019-04-09 17:33:12 +0100

    divide by 0

---
+++ src/DivideError.java
@@ -0,0 +1,6 @@
+
+public class DivideError {
+	public int uncontrolled(int a) {
+		return 1/a;
+	}
+}
--- src/TryCatchApp.java
+++ src/TryCatchApp.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
 
 public class TryCatchApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		int ch, calc;
+		System.out.println("1: Devide by zero");
+		System.out.println("2: Open inexistent file");
+		
+		ch = System.in.read();
+		switch (ch) {
+		case '1':
+			DivideError d = new DivideError();
+			calc=d.uncontrolled(0);
+			break;
+		case '2':
+			System.out.println("2 not implemented");
+			break;
+		}
+		System.out.println("Main is done");
 	}
 
 }
